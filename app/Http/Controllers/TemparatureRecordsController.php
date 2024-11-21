dd834f1eb86d974c0d0c52e00921c4831da9951a
Jerry Molar Parrocha <j.parrocha@mlgcl.edu.ph>
2024-11-21 16:19:18 +0800

    commit ni jawa

--- app/Http/Controllers/TemparatureRecordsController.php
+++ app/Http/Controllers/TemparatureRecordsController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Temparature_records;
+use App\Models\Temperature_records;
 use Illuminate\Http\Request;
+use Pusher\Pusher;
 
-class TemparatureRecordsController extends Controller
+class TemperatureRecordsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -34,7 +36,7 @@ class TemparatureRecordsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Temparature_records $temparature_records)
+    public function show()
     {
         //
     }
@@ -42,7 +44,7 @@ class TemparatureRecordsController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Temparature_records $temparature_records)
+    public function edit()
     {
         //
     }
@@ -50,7 +52,7 @@ class TemparatureRecordsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Temparature_records $temparature_records)
+    public function update(Request $request)
     {
         //
     }
@@ -58,8 +60,61 @@ class TemparatureRecordsController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Temparature_records $temparature_records)
+    public function destroy()
     {
         //
+    }
+
+    public function temperatureReading(Request $request)
+    {
+        try {
+            $tempReading = $request->input('temperature');
+
+            if ($tempReading) {
+                $pusher = new Pusher(
+                    env('PUSHER_APP_KEY'),
+                    env('PUSHER_APP_SECRET'),
+                    env('PUSHER_APP_ID'),
+                    [
+                        'cluster' => env('PUSHER_APP_CLUSTER'),
+                        'useTLS' => true,
+                    ]
+                );
+
+                $pusher->trigger('temp_reading_channel', 'temp_reading_channel', [
+                    'message' => 'Temperature reading received',
+                    'status' => 200,
+                    'data' => $tempReading,
+                ]);
+
+                return response()->json([
+                    'message' => 'Temperature reading received',
+                    'data' => $tempReading,
+                ], 200);
+            } else {
+                $pusher = new Pusher(
+                    env('PUSHER_APP_KEY'),
+                    env('PUSHER_APP_SECRET'),
+                    env('PUSHER_APP_ID'),
+                    [
+                        'cluster' => env('PUSHER_APP_CLUSTER'),
+                        'useTLS' => true,
+                    ]
+                );
+
+                $pusher->trigger('temp_reading_channel', 'temp_reading_channel', [
+                    'status' => 404,
+                    'message' => 'No temperature reading!',
+                ]);
+
+                return response()->json([
+                    'message' => 'No temperature reading!',
+                ], 404);
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => $th->getMessage(),
+            ], 500);
+        }
     }
 }
